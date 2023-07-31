47d3e136bddc97615188274cd30001c842847c7e
boyerkelton <97959253+boyerkelton@users.noreply.github.com>
2023-07-31 21:55:13 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x78831ecf43191bcf81e32144f68e64721cbb58c9";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
